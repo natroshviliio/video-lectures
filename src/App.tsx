3c8a5e9a29d360206f063b51cf5250912945a8db
natroshviliio <ioane.natroshvili08b@geolab.edu.ge>
2026-02-06 02:26:31 +0400

    testing cookieParser

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
+
 function App() {
-    const [data, setData] = useState("");
-    const base_url = import.meta.env.VITE_API_URL;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(base_url + "/");
-            const data = await response.json();
-            console.log(data);
+    const login = async () => {
+        const response = await api.post("/login", { username, password });
+        console.log(await response.data);
+    };
 
-            setData(data);
-        };
-
-        fetchData();
-    }, []);
-
-    return <div>Hello world {data} </div>;
+    return (
+        <div className="flex justify-center">
+            <div className="rounded-lg border border-neutral-100 shadow-md p-3 w-1/5 mt-5 flex flex-col gap-y-3">
+                <h1 className="font-bold text-2xl text-center">Sign in</h1>
+                <input
+                    type="text"
+                    className="w-2/3 py-2 px-3 text-lg outline-none border border-neutral-300 rounded-md mx-auto"
+                    placeholder="username"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className="w-2/3 py-2 px-3 text-lg outline-none border border-neutral-300 rounded-md mx-auto"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="py-3 px-5 rounded-md text-white bg-emerald-500 w-fit mx-auto cursor-pointer" onClick={login}>
+                    Sign in
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
