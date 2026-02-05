94e5851502054072cba961f102c7f96516819fdc
natroshviliio <ioane.natroshvili08b@geolab.edu.ge>
2026-02-05 20:45:27 +0400

    testing origin urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
-    const api = import.meta.env.VITE_API_URL;
+    const [data, setData] = useState("");
+    const base_url = import.meta.env.VITE_API_URL;
 
-    return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    {api} {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(base_url + "/");
+            const data = await response.json();
+            console.log(data);
+
+            setData(data);
+        };
+
+        fetchData();
+    }, []);
+
+    return <div>Hello world {data} </div>;
 }
 
 export default App;
